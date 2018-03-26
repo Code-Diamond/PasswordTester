f2bc24d9fcb66fb35ec25e5c5937d42b516d8734
JaminROCK <KingJamin@hotmail.com>
2018-03-26 14:25:21 -0500

    Update PasswordTester.cpp

--- PasswordTester.cpp
+++ PasswordTester.cpp
@@ -8,7 +8,6 @@ int main()
 
     while(continuation == 'y' || continuation == 'Y')
     {
-                //End of while loop
         //Create character arrays
         char numbers[10] = {'0','1','2','3','4','5','6','7','8','9'};
         char lowerCase[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
